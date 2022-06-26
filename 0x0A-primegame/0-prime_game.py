b0db0b8fd7dfac99fda950e5d25f9828ae9217bb
Mogakamo <mogaka.amo254@gmail.com>
2022-06-26 14:16:56 +0300

    Solved the error

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,28 +1,42 @@
 #!/usr/bin/python3
-""" Prime Game function """
+"""0. Prime Game - Maria and Ben are playing a game"""
 
 
 def isWinner(x, nums):
-    """ Maria and Ben are playing a game. Given a set of consecutive
-    integers starting from 1 up to and including n,
-    they take turns choosing a prime number from the set and
-    removing that number and its multiples from the set.
-    The player that cannot make a move loses the game.
-    They play x rounds of the game, where n may be different for each
-    round. Assuming Maria always goes first and both players play optimally,
-    determine who the winner of each game is"""
-    prime = 0
-    notPrime = 0
-    if nums and x > 0:
-        for i in nums:
-            if (i > 0):
-                if(i % 2 == 0):
-                    prime += 1
-                else:
-                    notPrime += 1
-        if prime >= notPrime:
-            return "Maria"
-        else:
-            return "Ben"
-    else:
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
         return None
+    if x != len(nums):
+        return None
+
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
